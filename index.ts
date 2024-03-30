8ad1410fe7695aaf4457ed226ccfa730f13c6761
MR Bhutto <160281510+Qadeerbhutto2@users.noreply.github.com>
2024-03-30 21:59:24 +0500

    this is Cli guessing game
    cli guessing game

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor (Math.random()*5+1);
+console.log(`wellcome to my game`);
+const answers = await inquirer.prompt([{
+    name: "userGuessedNumber",
+     type: "number",
+    message: "please select guess number 1-5",
+
+},
+
+]);
+if (answers.userGuessednumber === randomNumber ) {
+console.log(`well done MR/MS you guessed a right number.`);
+}else{
+    console.log(`SORRY MR/MS you guees a wrong number`)}
+    
